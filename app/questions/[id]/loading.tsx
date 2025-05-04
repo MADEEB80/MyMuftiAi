5e92d65e2c473bee0791533acddfedf1b9ffb89e
MADEEB80 <101536848+MADEEB80@users.noreply.github.com>
2025-05-04 18:50:57 +0500

    remove all

--- app/questions/[id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from "lucide-react"
-
-export default function QuestionDetailLoading() {
-  return (
-    <div className="container flex h-[calc(100vh-200px)] items-center justify-center">
-      <Loader2 className="h-8 w-8 animate-spin text-primary" />
-    </div>
-  )
-}
